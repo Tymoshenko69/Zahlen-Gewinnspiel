f6c9b05d9a55e85f9568fac5abc83342fc87e880
Tymoshenko69 <tymoshenko@student.tgm.ac.at>
2026-09-24 21:59:01 +0200

    Controller-Verdrahtung und Event-Handling v1.0

--- GewinnController.java
+++ GewinnController.java
@@ -1,3 +1,71 @@
+import javax.swing.*;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class GewinnController {
+    private GewinnModel model;
+    private GewinnView view;
     
+    public GewinnController(GewinnModel model, GewinnView view) {
+        this.model = model;
+        this.view = view;
+
+        this.view.getTxtSpielerZahl().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                verarbeiteEingabe();
+            }
+        });
+
+        this.view.getBtnReset().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                resetRunde();
+            }
+        });
+    }
+
+    private void verarbeiteEingabe() {
+        String input = view.getTxtSpielerZahl().getText().trim();
+        try {
+            int zal = Integer.parseInt(input);
+            if (zal < 1 || zal > 9) {
+                JOptionPane.showMessageDialog(view, "Bitte eine Zahl zwischen 1 und 9 eingeben!");
+                return;
+            }
+
+            model.berechneRunde(zal);
+            
+            view.getTxtComputerZahl().setText(String.valueOf(model.getComputerZahl()));
+            view.getLblGesamtpunkte().setText(String.valueOf(model.getGesamtPunkte()));
+
+            if (model.hatGewonnen()) {
+                view.getLblRundenErgebnis().setText("Gewonnen!");
+            } else if (model.hatVerloren()) {
+                view.getLblRundenErgebnis().setText("Verloren!");
+            } else {
+                int erg = model.getRundenErgebnis();
+                view.getLblRundenErgebnis().setText(erg > 0 ? "+" + erg : String.valueOf(erg));
+            }
+
+        } catch (NumberFormatException ex) {
+            JOptionPane.showMessageDialog(view, "Ungültige Eingabe! Bitte eine Zahl eingeben.");
+        }
+    }
+
+    private void resetRunde() {
+        view.getTxtSpielerZahl().setText("");
+        view.getTxtComputerZahl().setText("");
+        view.getLblRundenErgebnis().setText("");
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            GewinnModel model = new GewinnModel();
+            GewinnView view = new GewinnView();
+            new GewinnController(model, view);
+            view.setVisible(true);
+        });
+    }
 }
